d20c95b2e9b6103544fd2286d0f8936090d072d7
Kaio Breno <kbreno.martins.s@gmail.com>
2026-06-29 07:02:01 -0300

    foi feito uma pequena parte do JS que pode melhorar bastante e alguns ajustes no css e html

---
+++ assets/itens.js
@@ -0,0 +1,6 @@
+let itensJason = [
+    {id: 1, item: "boné", img: "../img/boné.png", price: 22.33},
+    {id: 2, item: "calça", img: "../img/calça.png", price: 88.99},
+    {id: 3, item: "camisa", img: "../img/camisa.png", price: 66},
+    {id: 4, item: "sapato", img: "../img/carrinho.png", price: 244.99}
+]
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,38 @@
-const c = (el) => document.querySelector(el);
-let car = [];
-let lista = c('.lista');
 
-c('.btnAdd').addEventListener('click', () => {
-    let item = c('#item').value;
-    let preco = c('#preco').value;
+let itensJason = [
+    {id: 1, item: "boné", img: "img/boné.png", price: 22.33},
+    {id: 2, item: "calça", img: "img/calça.png", price: 88.99},
+    {id: 3, item: "camisa", img: "img/camisa.png", price: 66},
+    {id: 4, item: "sapato", img: "img/sapato.png", price: 244.99}
+]
 
-    let li = document.createElement('li');
-    li.textContent = `${item} - R$ ${preco}`;
-    lista.appendChild(li);
-    c('#item').value = '';
-    c('#preco').value = '';
-    let teste = car.push({ item, preco });
-    console.log(teste);
+
+const c = (el) => document.querySelector(el)
+let cart = [];  
+
+itensJason.map((item, index) => {
+    let itensShop = c('.shop').cloneNode(true);
+
+    itensShop.setAttribute('data-key', index);
+
+    itensShop.querySelector('.shop-img').style.backgroundImage = `url('${item.img}')`;
+    itensShop.querySelector('.item-name').innerText = item.item;
+    itensShop.querySelector('.item-price').innerText = `R$ ${item.price.toFixed(2)}`;
+
+    
+    
+    
+    // FUNÇÃO PARA O BOTÃO ADICIONAR
+    itensShop.querySelector('.btnAdd').addEventListener('click', () => {
+        cart.push(item);
+        c('.item-car').innerHTML = cart.map(i => i.item).join(', ');
+    });
+
+
+
+
+
+    
+    c('.compra').append(itensShop);
 })
+c('.shop').remove();
